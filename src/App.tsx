d6512f34f6414d568739c54b63d340fab867c745
alexander050211 <alex0211@snu.ac.kr>
2024-09-09 17:49:30 +0900

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,27 @@
-import './App.css';
+import "./App.css";
 
-import { useState } from 'react';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+import profile from "./assets/react.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wrapper">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img src={profile} className="profileImage" alt="profile image" />
+        <p style={{ fontSize: "3em" }}>박인혁</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <ul>
+        <li>컴퓨터공학부 24학번</li>
+        <li>MBTI는 ISTP</li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
   );
 }
 
