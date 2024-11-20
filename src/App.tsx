cac9354d277b4f7ecb06d75c3e53a0e1edbfacc6
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2024-11-20 23:25:56 +0900

    prettier fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
-import "./App.css";
+import './App.css';
 
-import profile from "./assets/anxiety.jpg";
-import background from "./assets/IMG_5078.jpg";
+import profile from './assets/anxiety.jpg';
+import background from './assets/IMG_5078.jpg';
 
 function App() {
   return (
     <div className="wrapper">
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          backgroundImage: background
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundImage: background,
         }}
       >
         <img src={profile} className="profileImage" alt="profile image" />
-        <p style={{ fontSize: "3em" }}>박인혁</p>
+        <p style={{ fontSize: '3em' }}>박인혁</p>
       </div>
       <div>
         <ul>
           <li>컴퓨터공학부 24학번 05년생</li>
           <li>MBTI는 ISTP</li>
 
-          <li>취미는 PS(<a className="glowing-text-dynamic" href="https://solved.ac/profile/alex0211">백준</a>, <a className="glowing-text-dynamic" href="https://codeforces.com/profile/alex0211">코드포스</a>), 방탈출, 보드게임</li>
+          <li>
+            취미는 PS(
+            <a
+              className="glowing-text-dynamic"
+              href="https://solved.ac/profile/alex0211"
+            >
+              백준
+            </a>
+            ,{' '}
+            <a
+              className="glowing-text-dynamic"
+              href="https://codeforces.com/profile/alex0211"
+            >
+              코드포스
+            </a>
+            ), 방탈출, 보드게임
+          </li>
           <li>많이 배우고 싶습니다! 잘 부탁드립니다!</li>
         </ul>
       </div>
